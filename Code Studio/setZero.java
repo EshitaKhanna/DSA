e9b2b66273ce9354cc05f2caa3733603b7d9181d
Eshita Khanna <eshita.khanna97@gmail.com>
2023-10-02 22:58:01 +0530

    setZeros

---
+++ Code Studio/setZero.java
@@ -0,0 +1,38 @@
+public class setZero {
+	public static void setZeros(int matrix[][]) {
+		// Write your code here..
+		int n = matrix.length;
+		int m = matrix[0].length;
+
+		boolean row[] = new boolean[n];
+		boolean col[] = new boolean[m];
+
+		for (int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(matrix[i][j] == 0){
+					row[i] = true;
+					col[j] = true;
+				}
+			}
+		}
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(row[i] ==true || col[j] ==true){
+					matrix[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		int [][] matrix = {{7,0,3}, {4,21,0}};
+		setZeros(matrix);
+		for(int i=0; i<matrix.length; i++) {
+			for(int j=0; j<matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}

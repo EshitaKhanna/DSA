f815c257cd469fdbddd1287b1bb3a87bd02704a8
Eshita Khanna <eshita.khanna97@gmail.com>
2023-11-13 20:19:56 +0530

    NumberOfGoodPairs

---
+++ Leetcode/NumberOfGoodPairs.java
@@ -0,0 +1,30 @@
+/*
+ * 2824. Count Pairs Whose Sum is Less than Target
+ * https://leetcode.com/problems/number-of-good-pairs/description/
+*/
+
+package DSA.Leetcode;
+
+public class NumberOfGoodPairs{
+    public static int numIdenticalPairs(int[] nums) {
+
+        int count = 0;
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                if(i<j && nums[i] == nums[j]){
+                    count ++;
+                }
+            }
+
+        }
+
+        return count;
+        
+    }
+    public static void main(String[] args){
+
+        int [] arr = {1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(arr)); // output = 4
+
+    }
+}

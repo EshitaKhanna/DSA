11eefb53e6d536b8a500d865c201efb1e82aaa3a
Eshita Khanna <eshita.khanna97@gmail.com>
2023-11-13 20:04:16 +0530

    Two Sum

---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,29 @@
+/*
+ * 2824. Count Pairs Whose Sum is Less than Target
+ * https://leetcode.com/problems/two-sum/description/    
+*/
+
+package DSA.Leetcode;
+
+public class TwoSum{
+    public static int[] twoSum(int []arr, int k){
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j= i+1; j < arr.length; j++){
+                 if(arr[i] + arr[j] == k){
+                    return new int[] {i,j};
+                 }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String []args){
+        int[] arr = {2,7,11,15};
+        int [] ans = twoSum(arr, 9);
+        for(int i: ans){
+            System.out.print(i + " ");
+        }
+            
+    }
+}
